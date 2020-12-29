9a08f8c01666764b40ec846cce6a0e104407cc68
rutwik-shete <rutwik.shete@gmail.com>
2020-12-29 16:06:38 +0530

    Adding Cros Origin Annotation On Customer Controller As Post Method On Heroku Are Not Running

--- src/main/java/com/rutwik/farmerohfarmer/Controllers/CustomerController.java
+++ src/main/java/com/rutwik/farmerohfarmer/Controllers/CustomerController.java
@@ -1,6 +1,7 @@
 package com.rutwik.farmerohfarmer.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,7 @@ import com.rutwik.farmerohfarmer.Repositories.LocationsRepository;
 import com.rutwik.farmerohfarmer.Repositories.OrderRepository;
 import com.rutwik.farmerohfarmer.Repositories.ProductRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/customer")
 public class CustomerController {
