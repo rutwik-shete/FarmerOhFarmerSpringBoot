81db6b1a7ea722c9e007373753761248bbd899a8
rutwik-shete <rutwik.shete@gmail.com>
2021-01-14 20:26:00 +0530

    Added @CrossOrigin Annotation in product controller and farmer controller to access the api form another domain

--- src/main/java/com/rutwik/farmerohfarmer/Controllers/ProductController.java
+++ src/main/java/com/rutwik/farmerohfarmer/Controllers/ProductController.java
@@ -7,12 +7,14 @@ import com.rutwik.farmerohfarmer.Models.ProductData;
 import com.rutwik.farmerohfarmer.Repositories.ProductDataRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
